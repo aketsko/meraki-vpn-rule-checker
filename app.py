bb0b09c597a32d6bdc5c6f033db5fe7a9dbf622b
aketsko <alexander.ketsko@gmail.com>
2025-04-04 21:28:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -227,10 +227,22 @@ with st.sidebar.expander("🎛️ Rule Highlighting Colors", expanded=False):
         return st.color_picker(label, value=default_hex)
 
     highlight_colors = {
-        "exact_allow": color_slider("Described traffic is fully ALLOWED", "#09BC8A",help="No rule after this one will affec the traffic"),
-        "exact_deny": color_slider("Described traffic is fully DENIED", "#DA2C38",help="No rule after this one will affec the traffic"),
-        "partial_allow": color_slider("Described traffic is partially ALLOWED", "#99E2B4",help="This rule can affect the described traffic, to investigate further please make the search more specific"),
-        "partial_deny": color_slider("Described traffic is partially DENIED", "#F7EF81",help="This rule can affect the described traffic, to investigate further please make the search more specific")
+        "exact_allow": color_slider(
+            "Described traffic is fully ALLOWED", "#09BC8A",
+            "No rule after this one will affect the traffic."
+        ),
+        "exact_deny": color_slider(
+            "Described traffic is fully DENIED", "#DA2C38",
+            "No rule after this one will affect the traffic."
+        ),
+        "partial_allow": color_slider(
+            "Described traffic is partially ALLOWED", "#99E2B4",
+            "This rule can affect the described traffic. To investigate further, please make the search more specific."
+        ),
+        "partial_deny": color_slider(
+            "Described traffic is partially DENIED", "#F7EF81",
+            "This rule can affect the described traffic. To investigate further, please make the search more specific."
+        )
     }
 
     # Save colors in session state for access elsewhere
