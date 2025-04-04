f669621a723b027eb52b7df493f5ebe13ac29787
aketsko <alexander.ketsko@gmail.com>
2025-04-04 13:51:08 +0200

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import json
 
+def load_json_file(uploaded_file):
+    if uploaded_file:
+        return json.load(uploaded_file)
+    return None
+    
 def safe_dataframe(data):
     df = pd.DataFrame(data)
     for col in df.columns:
