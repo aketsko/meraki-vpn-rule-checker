04334571f00031b1cfc6cd3d8996923d2bb340ea
aketsko <alexander.ketsko@gmail.com>
2025-04-04 13:26:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from streamlit_searchbox import st_searchbox
 from utils.file_loader import load_json_file
 from utils.helpers import safe_dataframe, get_object_map, get_group_map, id_to_name
 from utils.match_logic import resolve_to_cidrs, match_input_to_rule, is_exact_subnet_match
-from API_Calls import fetch_meraki_data
+from utils.API_Calls import fetch_meraki_data
 
 # ------------------ PAGE SETUP ------------------
 st.set_page_config(page_title="Meraki VPN Rule Checker", layout="wide", page_icon="🛡️", initial_sidebar_state="expanded")
