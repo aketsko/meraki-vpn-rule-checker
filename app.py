7a4d4e6d86003198341690c935e05e9b53c8482d
aketsko <alexander.ketsko@gmail.com>
2025-04-05 02:21:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ highlight_colors = {
 # ------------------ STREAMLIT TABS ------------------
 st.header("🔎-🛡️-🧠 Choose the module:")
 # -------------- MANUAL TAB HANDLING ----------------
-tab_names = ["🛡️ Rule Checker", "🔎 Object & Group Search", "🧠 Optimization Insights"]
+tab_names = ["🔎 Object & Group Search", "🛡️ Rule Checker", "🧠 Optimization Insights"]
 
 if "active_tab" not in st.session_state:
     st.session_state.active_tab = tab_names[1]  # Object & Group Search"
