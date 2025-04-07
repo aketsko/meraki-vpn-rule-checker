086a0bbb363772655524769896435fdd50e30b57
aketsko <alexander.ketsko@gmail.com>
2025-04-07 19:08:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -443,7 +443,8 @@ with st.container():
         col_r.metric("🛡️ VPN Rules", f"{len(rules_data)}")
         col_o.metric("🌐 Objects", f"{len(objects_data)}")
         col_g.metric("🗃️ Groups", f"{len(groups_data)}")
- #       col_n.metric("🏢 Networks", len(st.session_state.get("extended_data", {}).get("networks", [])))
+        extended_networks = st.session_state.get("extended_data", {}).get("networks", [])
+        col_n.metric("🏢 Networks", len(extended_networks))
 
 # Update active_tab variable
 selected_tab = st.session_state.active_tab
