84ffb65cacb79ddbde99753942c013fcbc3c3cf2
aketsko <alexander.ketsko@gmail.com>
2025-04-08 21:54:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -929,7 +929,7 @@ elif selected_tab == "🛡️ Rule Checker":
         # ---------- LOCAL + VPN ----------
         elif show_local_and_vpn:
             count = len(shared_locs)
-            with st.expander(f"<span style='font-size:20px;'>🏠 Local Firewall Rules - `{count}`</span>", expanded=expand_all_local):
+            with st.expander(f"🏠 **Local Firewall Rules - {count}**", expanded=expand_all_local):
                 st.markdown("", unsafe_allow_html=True)
                 for location in sorted(shared_locs):
                     for net_id, info in extended_data.get("network_details", {}).items():
