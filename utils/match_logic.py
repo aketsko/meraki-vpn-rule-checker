562496d65aebf20b980924467070db175ac86737
aketsko <alexander.ketsko@gmail.com>
2025-04-11 13:34:41 +0200

    Update match_logic.py

--- utils/match_logic.py
+++ utils/match_logic.py
@@ -1,18 +1,37 @@
 import ipaddress
 
-def build_object_location_map(object_map, group_map, extended_data):
+def build_object_location_map(objects_data, groups_data, extended_data):
     object_location_map = {}
-    for net in vpn_settings:
-        net_name = net.get("name")
-        for subnet in net.get("subnets", []):
-            cidr = subnet.get("localSubnet")
-            use_vpn = subnet.get("useVpn", False)
-            if cidr:
-                # Add to map
-                object_location_map.setdefault(cidr, []).append({
-                    "network": net_name,
-                    "useVpn": use_vpn
-                })
+    vpn_subnets_per_network = {}
+
+    for net_id, details in extended_data.get("network_details", {}).items():
+        network_name = details.get("network_name", "")
+        subnets = details.get("vpn_settings", {}).get("subnets", [])
+        cidrs = [s.get("localSubnet", "") for s in subnets if "localSubnet" in s]
+        vpn_subnets_per_network[network_name] = cidrs
+
+    # Map object CIDRs to all matching networks
+    for obj in objects_data:
+        cidr = obj.get("cidr")
+        if not cidr:
+            continue
+        try:
+            obj_net = ipaddress.ip_network(cidr, strict=False)
+        except Exception:
+            continue
+
+        matching_networks = set()
+        for net_name, vpn_subnets in vpn_subnets_per_network.items():
+            for subnet in vpn_subnets:
+                try:
+                    vpn_net = ipaddress.ip_network(subnet, strict=False)
+                    if obj_net.subnet_of(vpn_net) or obj_net == vpn_net:
+                        matching_networks.add(net_name)
+                        break
+                except:
+                    continue
+        if matching_networks:
+            object_location_map[cidr] = sorted(matching_networks)
     # Add 0.0.0.0/0 mapped to all known subnets (catch-all)
     all_entries = []
     for cidr, entries in object_location_map.items():
